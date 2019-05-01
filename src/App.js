6b01675733c38c9d43a36e910f387b725dcb50fe
Cory House <housecor@gmail.com>
2019-05-01 18:53:51 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and stuff again and again. just again.
+          Learn React and stuff again and again. just again. agnadgd
         </a>
       </header>
     </div>
