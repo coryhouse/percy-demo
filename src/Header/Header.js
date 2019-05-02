483d8d9778db9d28b5474a532f0085b8eb094e17
Cory House <housecor@gmail.com>
2019-05-02 14:14:53 -0500

    Add header and footer

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav>
+      <a href="/">Home</a> | <a href="/about">About</a>
+    </nav>
+  );
+};
+
+export default Header;
