7599c223271166e47282cf496db9f339619e3079
Cory House <housecor@gmail.com>
2019-05-03 09:42:22 -0500

    Change app

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React again and again???
+          Learn React?
         </a>
       </header>
       <Footer />
