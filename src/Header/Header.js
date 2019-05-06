ca1ba9b74772e5473bcd6fa1b66ada2d528bfc8b
Cory House <housecor@gmail.com>
2019-05-06 09:14:41 -0500

    Change stuff

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <nav>
-      <a href="/">Home</a> | <a href="/about">About</a>
+      <a href="/">Home</a> | <a href="/about">About?</a>
     </nav>
   );
 };
